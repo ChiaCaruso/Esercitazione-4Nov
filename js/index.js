f1b994a478e98c6eb67cfee19b4b7714a9eae251
Chiara Caruso <carusochiara08@gmail.com>
2021-11-04 17:59:25 +0100

    Widget terminato

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
 
 /** 
- * Wraps the document.querySelector method
+ * Wraps the document.querySelector method.
  */
 
 const query = (selector) => document.querySelector(selector);
 
+
+/** 
+ * Populate html with data's elements.
+ */
+
 const render = (container, items) => {
     const elements = items.map( (element, index) => {
-        // return `${element.name}, ${element.phone}, ${element.email}`;
         return `<li>
         <h3>${element.name}</h3>
         <p><strong>Phone: </strong><a href="tel:${element.phone}">${element.phone}</a></p>
@@ -20,24 +24,24 @@ const render = (container, items) => {
     container.innerHTML = content;
 }
 
+
+
+
+/** 
+ * - Add listener;
+ * - Arrow function to delete last child of data.
+ */
+
 document.addEventListener('DOMContentLoaded', () => {
     const form = query("form");
     const input = query("form input");
     const list = query("ul");
+    const inputName = query("#name");
+    const inputEmail = query("#email");
+    const inputPhone = query("#phone");
+    const btnInsert = query(".btn-insert");
 
     render(list, data);
-
-    // Versione originale form
-    // form.addEventListener( "submit", (event) => {
-    //     event.preventDefault();
-    //     const value = input.value.toLowerCase();
-
-    //     const result = data.filter( (element) => {
-    //         return element.name.toLowerCase().search(value) > -1;
-    //     })
-
-    //     render(list, result);
-    // });
 
     input.addEventListener( "keyup", (event) => {
         event.preventDefault();
@@ -49,9 +53,39 @@ document.addEventListener('DOMContentLoaded', () => {
         )
 
         render(list, result);
+    });   
+
+    // const inputNameCapitalize = (value) => {
+    //     const inputLower = inputName.value.split(" ");
+    //     for (let index = 0; index < inputLower.length; index++) {
+    //         inputLower[index] = inputLower[index].charAt(0).toUpperCase() + inputLower[index].slice(1);
+    //     }
+
+        
+    // }
+    // function capitalizeFirstLetter(string) {
+
+    //     return string.charAt(0).toUpperCase() + string.slice(1);
+    //   }
+      
+    // console.log(capitalizeFirstLetter(inputName.value));
+
+
+
+    btnInsert.addEventListener( "click", (event) => {
+        event.preventDefault();
+
+        const newData = {};
+
+        newData.name = inputName.value;
+        newData.phone = inputPhone.value;
+        newData.email = inputEmail.value;
+
+        data.push(newData);
+
+        render(list, data);
+
     });
+      
 
-
-    
-
-});
+})
